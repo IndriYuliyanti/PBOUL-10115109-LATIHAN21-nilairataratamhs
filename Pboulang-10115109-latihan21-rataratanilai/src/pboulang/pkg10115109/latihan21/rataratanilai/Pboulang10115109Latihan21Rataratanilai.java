858d5a836a908a0866adf7f7568a60c98e0d5047
IndriYuliyanti <55839650+IndriYuliyanti@users.noreply.github.com>
2019-10-10 20:17:29 +0700

    Add files via upload

---
+++ Pboulang-10115109-latihan21-rataratanilai/src/pboulang/pkg10115109/latihan21/rataratanilai/Pboulang10115109Latihan21Rataratanilai.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10115109.latihan21.rataratanilai;
+import java.util.*;
+/**
+ *
+ * @author nama : indri yuliyanti
+ *          nim : 10115109
+ *          kls : pbo ulang
+ *      tugas : menghitung rata rata nilai mahasiswa
+ */
+public class Pboulang10115109Latihan21Rataratanilai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Masukan banyak mahasiswa: ");
+        
+        Scanner input = new Scanner(System.in);
+        int ukuran = input.nextInt();
+        
+        double array[] = new double[ukuran];
+        
+        double jumlah = 0;
+        
+        for (int i = 0; i < array.length; i++) {
+            
+            
+            System.out.printf("masukan mahasiswa ke-%d: ", (i + 1));
+            array[i] = input.nextDouble();
+        }
+        
+    for (int i = 0; i < array.length; i++){
+        jumlah = jumlah + array[i];
+    }
+    
+    double rataRata = jumlah / array.length;
+    
+        System.out.println("Maka, Rata-rata Nilainya adalah : " +rataRata);
+          
+        System.out.println("");
+        System.out.println("  develop BY: indri yuliyanti  ");
+    }
+    
+}
